3cc6b20116dd629e887f115b66314045dc6b506a
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-26 11:19:44 +0200

    Add: added Guestuser as model class

--- src/model/GuestUser.java
+++ src/model/GuestUser.java
@@ -1,5 +1,27 @@
 package model;
 
 public class GuestUser {
+	//mainīgie, getters, setters, abi konstruktori, toString + citas funkcijas
+	private long id;
+	
+	private static long counter = 0;
+
+	public long getId() {
+		return id;
+	}
+	
+	public void setId() {
+		id = counter++;
+	}
+	
+	public GuestUser() {
+		setId();
+	}
+	
+	public String toString() {
+		return id + "";
+	}
+	
+	//TODO pabeigt ar filtrācijas funkcijā
 
 }
