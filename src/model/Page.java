e840748c2f7188f6f1a0bd79d834628efb8cc221
KarinaSkirmante <karina.krinkele@gmail.com>
2025-02-28 10:02:56 +0200

    Add: added page class with variables, getters un setters

---
+++ src/model/Page.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Page {
+	//mainīgie, getters, setters, abi konstruktori, toString + citas funkcijas
+	private String title;
+	private String description;
+	private ArrayList<RegisterUser> allFollowers = new ArrayList<RegisterUser>();
+	private ArrayList<Post> allPostsInPage = new ArrayList<Post>();
+	
+	public String getTitle() {
+		return title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public ArrayList<RegisterUser> getAllFollowers() {
+		return allFollowers;
+	}
+	public ArrayList<Post> getAllPostsInPage() {
+		return allPostsInPage;
+	}
+	
+	public void setTitle(String inputTitle) {
+		if(inputTitle != null && inputTitle.matches("[A-Za-z ]{5,30}")) {
+			title = inputTitle;
+		}
+		else
+		{
+			title = "No title";
+		}
+	}
+	
+	public void setDescription(String inputDescription) {
+		if(inputDescription != null && inputDescription.matches("[A-Za-z0-9 !?]{5,100}"))
+		{
+			description = inputDescription;
+		}
+		else
+		{
+			description = "No description";
+		}
+	}
+	
+
+}
